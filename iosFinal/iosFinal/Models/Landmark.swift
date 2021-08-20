c0ad931c9e5c4100d0f1dd9e42673f37587b2679
Vincent <vincent_coutuer@yahoo.com>
2021-08-20 14:37:43 +0200

    Map fixes
    Added different picture for a cocktailbar, also possible to navigate to a bar with google maps

--- iosFinal/iosFinal/Models/Landmark.swift
+++ iosFinal/iosFinal/Models/Landmark.swift
@@ -12,19 +12,15 @@ import MapKit
 struct Landmark {
     
     let placemark: MKPlacemark
-    
     var id: UUID{
         return UUID()
     }
-    
     var name: String{
         self.placemark.name ?? ""
     }
-    
     var title: String {
         self.placemark.title ?? ""
     }
-    
     var coordinate: CLLocationCoordinate2D{
         self.placemark.coordinate
     }
--- iosFinal/iosFinal/ViewControllers/MapViewController.swift
+++ iosFinal/iosFinal/ViewControllers/MapViewController.swift
@@ -14,11 +14,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     @IBOutlet var mapView: MKMapView!
     
     let manager = CLLocationManager()
+    var isForCurrent = false
     private var landmarks: [Landmark] = [Landmark]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+      
   
     }
     
@@ -34,6 +35,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
+           // self.getNearByLandmarks()
             manager.stopUpdatingLocation()
             render(location)
         }
@@ -43,44 +45,85 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
                                                 longitude: location.coordinate.longitude)
-        
         let span = MKCoordinateSpan(latitudeDelta: 0.01,
                                     longitudeDelta: 0.01)
-        
         let region = MKCoordinateRegion(center: coordinate,
                                         span: span)
         mapView.delegate = self
         mapView.setRegion(region,
                           animated: true)
-        
         let pin = MKPointAnnotation()
         pin.coordinate = coordinate
         pin.title = "Current location"
-        
+        self.isForCurrent = true
         mapView.addAnnotation(pin)
-        self.getNearByLandmarks()
-        updateAnnotations(from: mapView)
+     
+       // updateAnnotations(from: mapView)
+    }
+//    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+//        print("clciked")
+//    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("click")
     }
 
-
     
+    func showMap(lat: String, lng: String) {
+        let browserDomen: String = "https://www.google.co.in/maps/dir/"
+        let directionBody: String = "?saddr=&daddr=\(lat),\(lng)&directionsmode=driving"
+        let url = URL(string: "comgooglemaps://")
+        if(UIApplication.shared.canOpenURL(url!)){
+            let locationUrl = URL(string: "comgooglemaps://?center=\(lat),\(lng)&zoom=14&views=traffic")
+            UIApplication.shared.open(locationUrl!)
+        }
+        else{
+            let browserUrl = URL(string: browserDomen)!
+            if(UIApplication.shared.canOpenURL(browserUrl)) {
+                guard let browserFullPathUrl = URL(string: browserDomen + directionBody) else { return }
+                UIApplication.shared.open(browserFullPathUrl)
+            }
+            // self.showErrorAlert(title: "", message: "Google Maps Not Installed")
+        }
+    }
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        self.showMap(lat: "\(view.annotation!.coordinate.latitude)", lng: "\(view.annotation!.coordinate.longitude)")
+//        let capital = view.annotation as! Capital
+//        let placeName = capital.title
+//        let placeInfo = capital.info
+//
+//        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//        present(ac, animated: true)
+    }
+
     func mapView(_ mapViewIcon: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
         var annotationView = mapViewIcon.dequeueReusableAnnotationView(withIdentifier: "custom")
-        
         if annotationView == nil {
             annotationView = MKAnnotationView(annotation: annotation,
                                               reuseIdentifier: "custom")
             annotationView?.canShowCallout = true
+           
         }else{
-            annotationView?.annotation = annotation
+        
+            annotationView!.annotation = annotation
         }
-        
-        annotationView?.image = UIImage(named: "User")
-        
+        if(self.isForCurrent){
+            self.isForCurrent = false
+            annotationView?.image = UIImage(named: "User")
+        }
+        else{
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView!.rightCalloutAccessoryView = btn
+            annotationView?.image = UIImage(named: "shop")
+        }
+     
         return annotationView
     }
     
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        self.getNearByLandmarks()
+    }
 
     
 
@@ -88,8 +131,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     private func getNearByLandmarks(){
             let request = MKLocalSearch.Request()
             request.naturalLanguageQuery = "cocktail"
-        request.region = mapView.region
-            
+            request.region = mapView.region
             let search = MKLocalSearch(request: request)
             search.start{(response, error) in
                 if let response = response {
@@ -97,17 +139,18 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                     self.landmarks = mapItems.map{
                         Landmark(placemark: $0.placemark)
                     }
+                    self.updateAnnotations(from: self.mapView)
                 }
-                    
-                
             }
         }
     
     private func updateAnnotations(from mapView: MKMapView){
         let annotations = self.landmarks.map(LandmarkAnnotation.init)
         mapView.addAnnotations(annotations)
+       
     }
  
 
 }
+
 
